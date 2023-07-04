bfc44835bc2ac1bae6a341607500a3dcfdfe5e1e
yabbyk <137085911+yabbyk@users.noreply.github.com>
2023-07-04 14:39:31 +0300

    Add files via upload

---
+++ Find Target Indices After Sorting Array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        indices = []
+        sortedNums = sorted(nums)
+         
+        for k, num in enumerate(sortedNums):
+            if num == target:
+                indices.append(k)
+        return indices
+solution = Solution()
+nums = [1,2,5,2,3]
+target = 2
+indices = solution.targetIndices(nums,target)
+print(indices)
+
+         
+    
